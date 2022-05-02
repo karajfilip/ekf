fe392d3ec98424be88f31b9717989c9793d2e4bf
Marija Piliškić <marija.piliskic@gideonbros.ai>
2022-05-02 21:51:30 +0200

    Fix SM 5.1

--- project/src/assembly_kitting_states.py
+++ project/src/assembly_kitting_states.py
@@ -28,7 +28,7 @@ class CheckGripper(smach.State):
         self.act = Actuators.Actuators()
 
     def execute(self, ud):
-        if self.act.gripper_type() !=  'gripper_part':
+        if str(self.act.gripper_type().data) !=  'gripper_part':
             ud.gripper = 'gripper_tray'
             return 'gripper_part'
         else:
@@ -111,7 +111,7 @@ class CheckMoveableTray(smach.State):
         self.act = Actuators.Actuators()
 
     def execute(self, ud):
-        if self.act.gripper_type() !=  'gripper_tray':
+        if self.act.gripper_type().data !=  'gripper_tray':
             ud.gripper = 'gripper_tray'
             return 'changegripper'
         else:
